3b50661a1aea24646e6262ba3bab56af66b54fae
AakLak <30278646+AakLak@users.noreply.github.com>
2024-12-16 05:45:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const playButton = document.getElementById('playButton');
+
 playButton.addEventListener('touchstart', (event) => {
     event.preventDefault(); // Prevent default behavior
     playAudio();
@@ -8,7 +10,7 @@ playButton.addEventListener('click', (event) => {
 });
 
 function playAudio() {
-    const audio = new Audio('https://raw.githubusercontent.com/AakLak/YoHoZomBo/main/zomebocom.mp3');
+    const audio = new Audio('https://raw.githubusercontent.com/AakLak/YoHoZomBo/main/zomebocom.mp3'); // Use the raw URL
     audio.play()
         .then(() => {
             console.log("Audio is playing");
