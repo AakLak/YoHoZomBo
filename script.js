0fe8a0169b58ccb547bf460786cbaf26139abba2
AakLak <30278646+AakLak@users.noreply.github.com>
2024-12-16 05:37:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const playButton = document.getElementById('playButton');
-
 playButton.addEventListener('touchstart', (event) => {
-    event.preventDefault(); // Prevent default behavior
+    alert("Button pressed!"); // Check if this alert shows up
+    event.preventDefault();
     const audio = new Audio('https://raw.githubusercontent.com/AakLak/YoHoZomBo/main/zomebocom.mp3');
     audio.play()
         .then(() => {
